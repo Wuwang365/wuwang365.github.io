254d8be4348e86539a430d5401786baea668919d
wuwang365 <1125344818@qq.com>
2025-03-16 12:51:25 +0800

    change website style

--- homepage/src/views/HomeView.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-import Navbar from "../components/Navbar.vue"
-</script>
-
-<template>
-    <Navbar></Navbar>
-</template>
